5fa80e745c54fd47b783a20d736dd79bff0a9f71
ant013 <ant013@mail.ru>
2022-10-03 17:23:56 +0600

    Update .gitignore file

--- Package.swift
+++ Package.swift
@@ -13,27 +13,28 @@ let package = Package(
             targets: ["BitcoinCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/horizontalsystems/HdWalletKit.Swift.git", .upToNextMajor(from: "1.2.0")),
-        .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "1.4.0")),
+        .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/Brightify/Cuckoo.git", .upToNextMajor(from: "1.5.0")),
+        .package(url: "https://github.com/groue/GRDB.swift.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "10.0.0"),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "5.0.1")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.1")),
+
+        .package(url: "https://github.com/horizontalsystems/HdWalletKit.Swift.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/horizontalsystems/HsCryptoKit.Swift.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.1")),
-        .package(url: "https://github.com/groue/GRDB.swift.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/Brightify/Cuckoo.git", .upToNextMajor(from: "1.5.0")),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "5.0.1")),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "10.0.0"),
+        .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         .target(
             name: "BitcoinCore",
             dependencies: [
                 "BigInt", "RxSwift",
+                .product(name: "GRDB", package: "GRDB.swift"),
+                .product(name: "HdWalletKit", package: "HdWalletKit.Swift"),
                 .product(name: "HsCryptoKit", package: "HsCryptoKit.Swift"),
                 .product(name: "HsExtensions", package: "HsExtensions.Swift"),
                 .product(name: "HsToolKit", package: "HsToolKit.Swift"),
-                .product(name: "HdWalletKit", package: "HdWalletKit.Swift"),
-                .product(name: "GRDB", package: "GRDB.swift")
             ]
         ),
         .testTarget(
@@ -41,8 +42,8 @@ let package = Package(
             dependencies: [
                 "BitcoinCore",
                 "Cuckoo",
-                "Quick",
                 "Nimble",
+                "Quick",
                 .product(name: "RxBlocking", package: "RxSwift"),
             ]
         )
