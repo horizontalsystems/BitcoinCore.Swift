a2f351b171c1b636e7a69f4a388991634e4931fc
ant013 <ant013@mail.ru>
2023-03-31 15:29:25 +0600

    Show user agent for peers

--- Sources/BitcoinCore/Classes/Core/BitcoinCore.swift
+++ Sources/BitcoinCore/Classes/Core/BitcoinCore.swift
@@ -267,6 +267,7 @@ extension BitcoinCore {
                 peerStatus.append(("status", initialBlockDownload.isSynced(peer: peer) ? "synced" : "not synced"))
                 peerStatus.append(("host", peer.host))
                 peerStatus.append(("best block", peer.announcedLastBlockHeight))
+                peerStatus.append(("user agent", peer.announcedLastBlockHeight))
 
                 let tasks = peer.tasks
                 if tasks.isEmpty {
