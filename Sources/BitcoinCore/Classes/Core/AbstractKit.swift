5ffb9b05e853f2c46f5fe3882f6efedcdd98099b
ant013 <ant013@mail.ru>
2024-01-05 17:38:25 +0600

    Update method to use custom unspent_outputs

--- Sources/BitcoinCore/Classes/Core/AbstractKit.swift
+++ Sources/BitcoinCore/Classes/Core/AbstractKit.swift
@@ -41,15 +41,11 @@ open class AbstractKit {
         bitcoinCore.transaction(hash: hash)
     }
 
-    open func send(to address: String, value: Int, feeRate: Int, sortType: TransactionDataSortType, pluginData: [UInt8: IPluginData] = [:]) throws -> FullTransaction {
-        try bitcoinCore.send(to: address, value: value, feeRate: feeRate, sortType: sortType, unspentOutputs: nil, pluginData: pluginData)
-    }
-
-    open func send(to address: String, value: Int, feeRate: Int, sortType: TransactionDataSortType, unspentOutputs: [UnspentOutput], pluginData: [UInt8: IPluginData] = [:]) throws -> FullTransaction {
+    open func send(to address: String, value: Int, feeRate: Int, sortType: TransactionDataSortType, unspentOutputs: [UnspentOutputInfo]? = nil, pluginData: [UInt8: IPluginData] = [:]) throws -> FullTransaction {
         try bitcoinCore.send(to: address, value: value, feeRate: feeRate, sortType: sortType, unspentOutputs: unspentOutputs, pluginData: pluginData)
     }
 
-    public func send(to hash: Data, scriptType: ScriptType, value: Int, feeRate: Int, sortType: TransactionDataSortType, unspentOutputs: [UnspentOutput]) throws -> FullTransaction {
+    public func send(to hash: Data, scriptType: ScriptType, value: Int, feeRate: Int, sortType: TransactionDataSortType, unspentOutputs: [UnspentOutputInfo]?) throws -> FullTransaction {
         try bitcoinCore.send(to: hash, scriptType: scriptType, value: value, feeRate: feeRate, sortType: sortType, unspentOutputs: unspentOutputs)
     }
 
@@ -78,8 +74,8 @@ open class AbstractKit {
         return try bitcoinCore.sendInfo(for: value, toAddress: toAddress, feeRate: feeRate, unspentOutputs: outputs, pluginData: pluginData)
     }
 
-    open func maxSpendableValue(toAddress: String? = nil, feeRate: Int, pluginData: [UInt8: IPluginData] = [:]) throws -> Int {
-        try bitcoinCore.maxSpendableValue(toAddress: toAddress, feeRate: feeRate, pluginData: pluginData)
+    open func maxSpendableValue(toAddress: String? = nil, feeRate: Int, unspentOutputs: [UnspentOutputInfo]?, pluginData: [UInt8: IPluginData] = [:]) throws -> Int {
+        try bitcoinCore.maxSpendableValue(toAddress: toAddress, feeRate: feeRate, unspentOutputs: unspentOutputs, pluginData: pluginData)
     }
 
     open func maxSpendLimit(pluginData: [UInt8: IPluginData]) throws -> Int? {
